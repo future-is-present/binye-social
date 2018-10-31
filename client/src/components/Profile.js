850882fd7e94463c60abb9b66ab0188157f3bbc4
manolodewiner <manolosingular@gmail.com>
2018-10-31 09:19:24 +0100

    add Profile component and container

---
+++ client/src/components/Profile.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {Container } from 'semantic-ui-react'
+import '../index.css'
+
+
+class Profile extends Component {
+    render() {
+      return (
+        <div className="profile_list" >
+        <Container>
+        <h4> Name</h4>
+          </Container>
+        </div>
+      );
+    }
+}
+
+export default Profile;
